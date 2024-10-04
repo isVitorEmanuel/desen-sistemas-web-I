8fbe71fb98f663a34603be7a93ec14344ca85f5d
Vitor Emanuel <vitoralencar2706@gmail.com>
2024-10-03 22:34:32 -0300

    Finish the exercice!

--- exercices/exercice02/js/validatedInput.js
+++ exercices/exercice02/js/validatedInput.js
@@ -1,25 +1,45 @@
+let interval = null;
+let fbackContainer = document.querySelector('#feedback');
+
+/**
+ * This function set a timeout to clear the feedback's message.
+ */
+const timeOut = () => {
+    clearTimeout(interval);
+
+    interval = setTimeout(() => {
+        fbackContainer.textContent = '';
+    }, 4000);
+};
+
 const validatedInput = (nameField, ageField, courseField) => {
     const nameValidity = nameField.validity;
     const ageValidity = ageField.validity;
     const courseValidity = courseField.validity;
     
-    // TODO: 
     if (nameValidity.valueMissing) {
-        console.log("Preecha o Campo de Nome!");
+        fbackContainer.textContent = "campo nome é obrigatório!"
+        timeOut();
         return false;
     }
-
-    // TODO: 
-    if (courseValidity.valueMissing) {
-        console.log("Preencha o Campo de Curso!");
-        return false;
-    }
-
-    // TODO: 
-    if (false) {
-        console.log("Preencha corretamente o campo de idade!")
+    
+    if (ageValidity) {
+        if (ageField.validity.valueMissing) {
+            fbackContainer.textContent = "campo idade é obrigatório!"
+        } else if (ageField.validity.rangeUnderflow) {
+            fbackContainer.textContent = "idade deve ser maior que 13!"
+        } else if (ageField.validity.rangeOverflow) {
+            fbackContainer.textContent = "idade deve ser menor que 120!"
+        }
+        timeOut();
         return false;
     } 
+    
+    if (courseValidity.valueMissing) {
+        fbackContainer.textContent = "campo curso é obrigatório!"
+        timeOut();
+        return false;
+    }
     
     return true;
 };
